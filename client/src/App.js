596a2bc9fff942bc1ce0f48e17894e41c34bbe33
scream <hou.jennifer35@gmail.com>
2019-03-17 16:59:47 -0400

    display all kudos

--- client/src/App.js
+++ client/src/App.js
@@ -15,33 +15,48 @@ class App extends Component {
     receiver: ''
   }
 
-  getAllUsers = () => {
-    $.get('/api/users')
-    .then((res) => {
-      // console.log(res.data);
-      this.setState({ usersList: res.data });
-    });
-  }
-  
-  getAllkudos = () => {
-    $.get('/api/kudos')
-    .then((res) => {
-      this.setState({ kudosList: res.data });
-    });
+  getAllkudos = (data) => {
+    const kudoArray = [];
+    for(let i = 0; i < data.length; i++) {
+      const username = data[i].username;
+      const kudos = data[i].kudos;
+      for (let j = 0; j < kudos.length; j++) {
+        const clone = {...kudos[j]}
+        clone.username = username;
+        kudoArray.push(clone);
+      }
+      console.log(kudos);
+    }
+    console.log(kudoArray);
+    const sortById = (a, b) => {
+        let x = a._id.toLowerCase();
+        let y = b._id.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+    }
+    this.setState({ kudosList: kudoArray.sort(sortById) });
     console.log(this.state.kudosList);
   }
 
+  getAllUsers = () => {
+    $.get('/api/users')
+      .then((res) => {
+        this.setState({ usersList: res.data });
+        console.log(res.data);
+        this.getAllkudos(res.data);
+      });
+  }
+
   componentDidMount = () => {
-    this.getAllkudos();
     this.getAllUsers();
   }
 
   handleChange = e => {
     e.preventDefault();
     this.setState({
-      [e.target.name] : e.target.value
+      [e.target.name]: e.target.value
     });
-    console.log(this.state.title, this.state.body, this.state.sender, this.state.receiver);
   }
 
   handleClick = () => {
@@ -52,19 +67,17 @@ class App extends Component {
       toUser: this.state.receiver
     }
     $.post('/api/kudo', newKudo)
-    .then( (data) => {
-      console.log(data);
-      // this.setState({
-      //   kudosList: [...this.state.kudosList, data]
-      // });
-    });
+      .then((data) => {
+        console.log(data);
+        this.getAllUsers();
+      });
   }
 
   render() {
     return (
       <div className="App">
         <Container>
-          <Header 
+          <Header
             usersList={this.state.usersList}
             handleChange={this.handleChange}
             handleClick={this.handleClick}
@@ -72,7 +85,7 @@ class App extends Component {
           <Row>
             <Col></Col>
             <Col xs='8'>
-              <Kudo kudosList={this.state.kudosList}/>
+              <Kudo kudosList={this.state.kudosList} />
             </Col>
             <Col></Col>
           </Row>
--- client/src/components/Kudo.js
+++ client/src/components/Kudo.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+   Card, CardText, CardBody,
+   CardTitle, CardSubtitle
+} from 'reactstrap';
 
 const Kudo = (props) => (
-   <div>
+   <div>{props.kudosList.map(kudo => (
+      <Card key={kudo._id}>
+         <CardBody>
+            <CardTitle>{kudo.title}</CardTitle>
+            <CardSubtitle>{kudo.fromuser}</CardSubtitle>
+            <CardText>{kudo.body}</CardText>
+            <CardSubtitle>~Sommebody</CardSubtitle>
+         </CardBody>
+      </Card>
+   ))}
    </div>
 );
 
